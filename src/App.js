5d42e5c0bb9eb5f21b4a4c23b7c29228f9669644
moath al-abdulrazaq <moath@moaths-MacBook-Air.local>
2022-06-28 09:09:29 +0100

    almost done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [todos, setTodos] =  useState([
+  { name: "Buy shopping", priority: "high" },
+  { name: "Clean bathroom", priority: "low" },
+  { name: "Car's MOT", priority: "high" }
+  ])
+
+  const [priorityValue , setpriorityValue] = useState("") 
+  const [newTodo , setNewTodo] = useState("")
+  const todosNodes = todos.map((todo,index)=>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <li>
+    <span> {todo.name} </span> 
+    
+  </li>
+  )})
+
+
+  const handleTodoradio = (evt) => {
+    setpriorityValue(evt.target.value)
+
+  }
+
+
+  const handleTodoInput = (evt) => {
+    setNewTodo(evt.target.value)
+  }
+
+  const saveNewTodo = (evt) => {
+    evt.preventDefault() 
+
+    const copyTodos = [...todos]; 
+    copyTodos.push({name : newTodo, priority : handleTodoradio(evt)})
+    setTodos(copyTodos)
+    setNewTodo("")
+    setpriorityValue("")
+
+
+  }
+
+
+
+
+  return (
+    <div>
+
+      <h1>Todo List</h1>
+      <hr></hr>
+
+      <form onSubmit = {saveNewTodo}>
+        <input id='new-todo' type='text' value={newTodo} onChange = {handleTodoInput} />
+
+        <label htmlFor="high">High</label>
+        <input type="radio" id="high" name="high" value= "high" onSelect = {handleTodoradio} />
+
+        <label htmlFor="low">Low</label>
+        <input type="radio" id="low" name="low" value="low" onSelect = {handleTodoradio} />
+
+        <input type="submit" value = "save a new todo"/>
+
+
+      </form>
+
+
+
+
+      <ul>
+        {todosNodes}
+      </ul>
+
     </div>
   );
-}
+
+
+
+};
 
 export default App;
